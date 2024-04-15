68d65a92ab08823f9b17c0e56cc7a91b02613777
Devender Verma <85267939+crispy-coffee@users.noreply.github.com>
2024-04-15 21:52:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const PORT = 4000;
 
 app.get('/api', (req, res) => {
     try{        
-        return res.status(400).json({
+        return res.status(200).json({
             success:false,
         });
 
     }catch(err){
-        return res.status(400).json({
+        return res.status(200).json({
             success:false,
         });
     }
